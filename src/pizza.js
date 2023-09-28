a0c4cc162cdfbd8f3235488e6a6e4f18e17f0c0b
Teddy Yongo <teddy@aeeiee.com>
2023-09-28 23:30:45 +0300

    Conditional rendering of styles

--- src/pizza.js
+++ src/pizza.js
@@ -1,11 +1,14 @@
 function Pizza({ pizzaObj }) {
+	//This will be checking  all the data about the different types of foods.
 	console.log(pizzaObj);
+
 	return (
-		<li className="pizza">
+		<li className={`pizza ${pizzaObj.soldOut ? "sold-out" : ""}`}>
 			<img src={pizzaObj.photoName} alt={pizzaObj.name} />
 			<div>
 				<h3>{pizzaObj.name}</h3>
 				<p>{pizzaObj.ingredients}</p>
+				<span>{pizzaObj.soldOut ? "Sold Out" : pizzaObj.price}</span>
 			</div>
 		</li>
 	);
